2d1df0219c97d123cc5f9b0c7e0b45ab0cec8d46
Vincent Woo <me@vincentwoo.com>
2016-04-05 00:11:51 -0700

    Firepad Lite initial commit

--- test/specs/cursor.spec.js
+++ test/specs/cursor.spec.js
@@ -8,20 +8,4 @@ describe('Cursor Tests', function() {
     expect(cursor.position).toBe(3);
     expect(cursor.selectionEnd).toBe(5);
   });
-
-  it('Transform', function() {
-    var cursor = new Cursor(3, 7);
-    expect(cursor
-      .transform(new TextOperation().retain(3).insert('lorem')['delete'](2).retain(42))
-      .equals(new Cursor(8, 10))).toBeTruthy();
-    expect(cursor
-      .transform(new TextOperation()['delete'](45))
-      .equals(new Cursor(0, 0))).toBeTruthy();
-  });
-
-  it('Compose', function() {
-    var a = new Cursor(3, 7);
-    var b = new Cursor(4, 4);
-    expect(a.compose(b)).toBe(b);
-  });
 });
